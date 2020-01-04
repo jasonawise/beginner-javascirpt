461248499052ac32d7ac5611ca28c33f8ea922ad
Jason Wise <jasonandrewwise@gmail.com>
2020-01-04 10:58:15 -0500

    learning about console.dir

--- exercises/20 - The DOM/the-dom.js
+++ exercises/20 - The DOM/the-dom.js
@@ -1,4 +1,3 @@
-const p = document.querySelector('p');
-const divs = document.querySelectorAll('div');
-console.log(p);
-console.log(divs);
+const heading = document.querySelector('h2');
+
+console.dir(heading);
