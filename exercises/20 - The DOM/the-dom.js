a2334bad9e537b8ff7efa23332510db91bcae66a
Jason Wise <jasonandrewwise@gmail.com>
2020-01-05 20:39:05 -0500

    learning about insertadjacenttext

--- exercises/20 - The DOM/the-dom.js
+++ exercises/20 - The DOM/the-dom.js
@@ -1,3 +1,7 @@
 const heading = document.querySelector('h2');
 
 console.dir(heading);
+
+const pizzaList = document.querySelector('.pizza');
+console.log(pizzaList.textContent);
+pizzaList.insertAdjacentText('beforeend', 'pizza');
