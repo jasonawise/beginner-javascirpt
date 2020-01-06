c4582352555cc5c3634bd55ce866bc7a285b40f8
Jason Wise <jasonandrewwise@gmail.com>
2020-01-05 20:48:21 -0500

    Learning about class methonds

--- exercises/20 - The DOM/the-dom.js
+++ exercises/20 - The DOM/the-dom.js
@@ -1,7 +1,13 @@
-const heading = document.querySelector('h2');
+// const heading = document.querySelector('h2');
 
-console.dir(heading);
+// console.dir(heading);
 
-const pizzaList = document.querySelector('.pizza');
-console.log(pizzaList.textContent);
-pizzaList.insertAdjacentText('beforeend', 'pizza');
+// const pizzaList = document.querySelector('.pizza');
+// console.log(pizzaList.textContent);
+// pizzaList.insertAdjacentText('beforeend', 'pizza');
+
+// CLASSESS!!!!
+const pic = document.querySelector('.nice');
+pic.classList.add('open');
+pic.classList.toggle('round');
+console.log(pic.classList);
